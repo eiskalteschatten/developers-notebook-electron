f9858a40e7302551991938479021a8751c92d8e0
Alex <alex@alexseifert.com>
2018-10-20 00:12:14 +0200

    Add global scss files and the build script for them

---
+++ build-scripts/build.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const path = require('path');
+const compileSass = require('compile-sass');
+const fs = require('fs');
+
+const pathToScss = path.resolve('./src/assets/scss');
+
+
+async function build() {
+    try {
+        fs.readdir(pathToScss, async (error, files) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+
+            const scssFiles = [];
+
+            for (const file of files) {
+                if (file.charAt(0) !== '_' && path.extname(file) === '.scss')
+                    scssFiles.push(file);
+            }
+
+            await compileSass.compileSassAndSaveMultiple({
+                sassPath: pathToScss,
+                cssPath: path.resolve('./dist'),
+                files: scssFiles
+            });
+        });
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
+
+build();
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -5,13 +5,8 @@ new Vue({
     el: "#vueAnchor",
     template: `
     <div>
-        <div>Hello {{name}}!</div>
-        Name: <input v-model="name" type="text">
         <structure-component :name="name" :initialEnthusiasm="5" />
     </div>`,
-    data: {
-        name: "World"
-    },
     components: {
         StructureComponent
     }
