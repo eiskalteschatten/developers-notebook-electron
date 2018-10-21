0137a2d0172ff2b51ed9017ec0429c4eb91345a6
Alex <alex@alexseifert.com>
2018-10-21 23:54:34 +0200

    Add space

--- src/events/menu.js
+++ src/events/menu.js
@@ -3,6 +3,7 @@ import {ipcRenderer} from 'electron';
 import {eventBus} from '../app';
 import router from '../router';
 
+
 export default () => {
     ipcRenderer.on('open-route', (event, route) => {
         router.push(route);
