22c7c26966a3b859518270190be90cbe5c3b7050
Alex <alex@alexseifert.com>
2018-10-21 20:40:18 +0200

    Add context menu to all form inputs

--- src/menus/menus.js
+++ src/menus/menus.js
@@ -4,7 +4,15 @@ const {ipcMain} = require('electron');
 
 const {switchMenu} = require('../lib/preferences/theme');
 
+const inputCm = require('./config/inputCm');
+
 
 ipcMain.on('switch-theme', (event, theme) => {
     switchMenu(theme);
+});
+
+ipcMain.on('show-input-context-menu', event => {
+    const window = BrowserWindow.fromWebContents(event.sender);
+    const menu = Menu.buildFromTemplate(inputCm);
+    menu.popup(window);
 });
