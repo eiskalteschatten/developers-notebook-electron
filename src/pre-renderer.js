9d58da93123e9fe0563927d56b2e01e6a64e613b
Alex <alex@alexseifert.com>
2018-10-21 20:44:50 +0200

    Clean up code

--- src/pre-renderer.js
+++ src/pre-renderer.js
@@ -9,14 +9,6 @@ function preRender() {
 
         const body = document.getElementsByTagName('body')[0];
         body.classList.add(process.platform);
-
-        // let preferences = localStorage.getItem('preferences');
-
-        // if (preferences) {
-        //     preferences = JSON.parse(preferences);
-        //     document.getElementById('sidebarWrapper').style.width = `${preferences.sidebarWidth}px`;
-        //     document.getElementById('bookListWrapper').style.width = `${preferences.middleColumnWidth}px`;
-        // }
     }
     catch(error) {
         console.error(error);
