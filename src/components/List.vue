5279afce8ab94462e0a9327586270d09f5c010f1
Alex Seifert <alexander.seifert@jochen-schweizer.de>
2018-10-22 17:39:50 +0200

    Start list components

---
+++ src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-bind:class="{'scrollable': scrollable}">
+        <slot/>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        data() {
+            return {
+                scrollable: false
+            }
+        }
+    });
+</script>
+
+<style lang="scss" scoped>
+    .scrollable {
+        overflow: auto;
+        position: relative;
+    }
+</style>
--- src/components/Views/Categories.vue
+++ src/components/Views/Categories.vue
@@ -1,13 +1,22 @@
 <template>
     <div>
-        Categories
+        <list scrollable="false">
+            <list-item />
+        </list>
     </div>
 </template>
 
 <script>
     import Vue from 'vue';
 
+    import List from '../List.vue';
+    import ListItem from '../List/ListItem.vue';
+
     export default Vue.extend({
+        components: {
+            List,
+            ListItem
+        }
     });
 </script>
 
